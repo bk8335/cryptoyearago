692d0bd79185ac24448d48374705f6bf558f3ab5
bk8335 <bk8335@gmail.com>
2018-04-09 22:38:16 +0100

    btc price today and year ago and percentage change

--- Gemfile
+++ Gemfile
@@ -11,6 +11,8 @@ gem 'turbolinks',   '5.0.1'
 gem 'jbuilder',     '2.7.0'
 gem 'bootstrap-sass', '3.3.7'
 gem 'delayed_job_active_record'
+gem 'nokogiri'
+gem 'httparty'
 
 group :development, :test do
   gem 'sqlite3', '1.3.13'
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,17 @@
 class StaticPagesController < ApplicationController
+
+	require 'open-uri'
+
   def home
+  	json_now = JSON.parse(open("https://api.coindesk.com/v1/bpi/currentprice.json").read)
+  	@price_now = json_now['bpi']['USD']['rate'].delete(',').to_f
+  	
+  	start_date = Date.today-1.year
+  	end_date = start_date + 1.day
+  	
+  	json_year_ago = JSON.parse(open("https://api.coindesk.com/v1/bpi/historical/close.json?start=#{start_date}&end=#{end_date}").read)
+  	@price_year_ago = json_year_ago['bpi'].first.last
+
+  	@percentage_change = (((@price_now / @price_year_ago)-1)*100).round(1)
   end
 end
