7a2adbf7930e4fb33f60d8e84cf4c73423672c9b
bk8335 <bk8335@gmail.com>
2018-04-23 11:58:08 +0100

    styling for homepage

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,10 +6,10 @@ class StaticPagesController < ApplicationController
   	json_now = JSON.parse(open("https://api.coindesk.com/v1/bpi/currentprice.json").read)
   	@price_now = json_now['bpi']['USD']['rate'].delete(',').to_f
   	
-  	start_date = Date.today-1.year
-  	end_date = start_date + 1.day
+  	@start_date = Date.today-1.year
+  	end_date = @start_date + 1.day
   	
-  	json_year_ago = JSON.parse(open("https://api.coindesk.com/v1/bpi/historical/close.json?start=#{start_date}&end=#{end_date}").read)
+  	json_year_ago = JSON.parse(open("https://api.coindesk.com/v1/bpi/historical/close.json?start=#{@start_date}&end=#{end_date}").read)
   	@price_year_ago = json_year_ago['bpi'].first.last
 
   	@percentage_change = (((@price_now / @price_year_ago)-1)*100).round(1)
