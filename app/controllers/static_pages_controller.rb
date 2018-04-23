1b9853e1fd85f307d9d133b0298f736883927617
bk8335 <bk8335@gmail.com>
2018-04-23 13:59:49 +0100

    eth page done

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -14,4 +14,23 @@ class StaticPagesController < ApplicationController
 
   	@percentage_change = (((@price_now / @price_year_ago)-1)*100).round(1)
   end
+
+  def eth
+    @price_now = JSON.parse(open("https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD").read)['USD']
+    @start_date = Date.today - 1.year
+    @time_year_ago = @start_date.to_time.to_i
+    @price_year_ago = JSON.parse(open("https://min-api.cryptocompare.com/data/pricehistorical?fsym=ETH&tsyms=USD&ts=1492902000").read)['ETH']['USD']
+    @percentage_change = (((@price_now / @price_year_ago)-1)*100).round(1)
+  end
+
+  def bch
+    @price_now = JSON.parse(open("https://min-api.cryptocompare.com/data/price?fsym=BCH&tsyms=USD").read)['USD']
+    @start_date = Date.today-1.year
+  end
+
+  def ltc
+    @price_now = JSON.parse(open("https://min-api.cryptocompare.com/data/price?fsym=LTC&tsyms=USD").read)['USD']
+    @start_date = Date.today-1.year
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get '/bitcoin', to: 'static_pages#home'
   get '/btc', to: 'static_pages#home'
+  get '/eth', to: 'static_pages#eth'
+  get '/ethereum', to: 'static_pages#eth'
+  get '/bch', to: 'static_pages#bch'
+  get '/bitcoin-cash', to: 'static_pages#bch'
+  get '/ltc', to: 'static_pages#ltc'
+  get '/litecoin', to: 'static_pages#ltc'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
